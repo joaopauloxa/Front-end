7feb5501fa317c30ef09053ae1f2ad04840d680b
Joao Paulo <joaopaulo.xa@gmail.com>
2022-03-27 15:23:13 -0300

    comitando projeto

--- projetojava1/src/PrimeiroPrograma.java
+++ projetojava1/src/PrimeiroPrograma.java
@@ -4,6 +4,7 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("teste de código");
+      System.out.println("Após implementar GitHub");
 	}
 
 }
